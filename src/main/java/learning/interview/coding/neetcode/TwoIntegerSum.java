b140f8b53f358c2dc410fbfaf9260496fe5ac6c2
Ignasi Gros Lluch <ignasi.gros.lluch@gmail.com>
2024-06-08 15:48:08 +0200

    Added TwoIntegerSum

---
+++ src/main/java/learning/interview/coding/neetcode/TwoIntegerSum.java
@@ -0,0 +1,20 @@
+package learning.interview.coding.neetcode;
+
+import java.util.HashMap;
+
+public class TwoIntegerSum {
+
+    static class Solution1 {
+        public int[] twoSum(int[] nums, int target) throws Exception {
+            var map = new HashMap<Integer, Integer>(nums.length);
+            for (int i=0; i<nums.length; i++) {
+                int potentialPair = target - nums[i];
+                if (map.containsKey(potentialPair)) {
+                    return new int[]{map.get(potentialPair), i};
+                }
+                map.put(nums[i], i);
+            }
+            throw new Exception("There were no combinations");
+        }
+    }
+}
