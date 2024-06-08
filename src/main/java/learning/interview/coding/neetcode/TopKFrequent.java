16360044e19e44375dfe72323d7e569ba160340f
Ignasi Gros Lluch <ignasi.gros.lluch@gmail.com>
2024-06-08 21:40:00 +0200

    Added TopKFrequent

---
+++ src/main/java/learning/interview/coding/neetcode/TopKFrequent.java
@@ -0,0 +1,23 @@
+package learning.interview.coding.neetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TopKFrequent {
+
+    static class Solution1 {
+        public int[] topKFrequent(int[] nums, int k) {
+            var map = new HashMap<Integer, Integer>();
+            for (int i : nums) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+            return map.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .skip(map.size() - k)
+                    .map(Map.Entry::getKey)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        }
+    }
+}
