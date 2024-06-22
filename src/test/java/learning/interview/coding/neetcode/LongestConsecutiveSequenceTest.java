b20ecfdefc442a5815f36f83d4d22a11b12fbe62
Ignasi Gros Lluch <ignasi.gros.lluch@gmail.com>
2024-06-22 13:01:31 +0200

    Added LongestConsecutiveSequence

---
+++ src/test/java/learning/interview/coding/neetcode/LongestConsecutiveSequenceTest.java
@@ -0,0 +1,22 @@
+package learning.interview.coding.neetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LongestConsecutiveSequenceTest {
+
+    private final LongestConsecutiveSequence.Solution1 solution1 = new LongestConsecutiveSequence.Solution1();
+
+    @Test
+    void testSolution1() {
+        // given
+        int[] parameter = new int[]{2,20,4,10,3,4,5};
+
+        // when
+        int result = solution1.longestConsecutive(parameter);
+
+        // then
+        assertThat(result).isEqualTo(4);
+    }
+}
